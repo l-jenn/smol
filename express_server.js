8ec02addaf758a50f5c4ee2cb0a8bd276a4bc666
vagrant <lam-jennifer@outlook.com>
2022-01-25 04:40:44 +0000

    Installed Morgan for logging

--- express_server.js
+++ express_server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const cookieParser = require("cookie-parser");
+const morgan = require('morgan');
 
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
 
